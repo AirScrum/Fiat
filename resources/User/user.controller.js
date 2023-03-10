b687cc08017ea7a1f2ccfebeae6c30e8a5beb752
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-03-10 02:17:36 +0200

    Remove get profile details

--- resources/User/user.controller.js
+++ resources/User/user.controller.js
@@ -1,41 +1,18 @@
 // Important requires
 const UserModel = require("./user.model");
 
-// Function to get Profile details
-const getProfile = (req, res) => {
-
-    const userId = req.body.userid;
-
-    // Find the user by their ID in the database
-    // Find the user by their ID in the database
-    UserModel.findById(userId)
-        .select("-createdAt -updatedAt -__v -password -_id")
-        .then((user) => {
-            if (user) {
-                // If user is found, send all attributes back in response
-                res.send(user);
-            } else {
-                // If user is not found, send a 404 response
-                res.status(404).send("User not found");
-            }
-        })
-        .catch((err) => {
-            // Handle error
-            console.error(err);
-            res.status(500).send("Error finding user");
-        });
-};
-
 // Function to update Profile details
 const updateProfile = (req, res) => {
 
     const userId = req.body.userid;
     const userDetails = req.body;
+    console.log(userId)
 
     // Find the user by their ID and update their details
     UserModel.findByIdAndUpdate(userId, userDetails, { new: true })
         .select("-createdAt -updatedAt -__v -password")
         .then((user) => {
+
             if (user) {
                 // If user is found and updated, send the updated details back in response
                 res.send(user);
@@ -51,4 +28,4 @@ const updateProfile = (req, res) => {
         });
 };
 
-module.exports = { getProfile, updateProfile };
+module.exports = { updateProfile };
--- resources/User/user.router.js
+++ resources/User/user.router.js
@@ -1,9 +1,6 @@
-const { getProfile,updateProfile } = require('./user.controller')
+const { updateProfile } = require('./user.controller')
 
 module.exports = function (app) {
-
-  // Route to get Profile details
-  app.get("/profile", getProfile);
 
   // Route to update Profile details
   app.post("/profile", updateProfile);
