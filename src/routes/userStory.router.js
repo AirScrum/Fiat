402cc832880324d02164b4887e23c4c40b4a9ce1
Shehab Adel <shehabeldinadel@gmail.com>
2023-04-09 00:54:10 +0200

    Included updateOne and removeOne in userStories

--- src/routes/userStory.router.js
+++ src/routes/userStory.router.js
@@ -2,7 +2,17 @@
 import { Router } from "express";
 import userStoriesControllers from "../controllers/userStory.controller";
 const router = Router();
-router.route("/").get(userStoriesControllers.getAll)
-router.route("/history").post(userStoriesControllers.getHistory);
+/**
+ * Update userStory by userStoryID
+ * Delete userStory by userStoryID
+ */
+router.route("/:id")
+.put(userStoriesControllers.updateOne)
+.delete(userStoriesControllers.removeOne)
+/**
+ * Get userStories by meetingID && userID
+ */
+router.route("/history")
+.post(userStoriesControllers.getHistory);
 
 export default router;
