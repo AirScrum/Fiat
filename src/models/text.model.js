86cda96ba6720668238efcfee3a612932667db9e
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-05-07 23:23:51 +0300

    Added name to the text.model

--- src/models/text.model.js
+++ src/models/text.model.js
@@ -3,28 +3,32 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const textSchema = new Schema(
-  {
-    textContent: {
-      type: String,
-      required: true,
+    {
+        textContent: {
+            type: String,
+            required: true
+        },
+        userID: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: false
+        }
     },
-    userID: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
-textSchema.pre('findOneAndRemove',async function(){
-  /**
-   * Removing all userStories associated with 
-   * the deleted meeting.
-   */
-    var textID= this.getQuery()["_id"]
-    if(!textID){
-      throw new Error("No textID found!")
+textSchema.pre("findOneAndRemove", async function () {
+    /**
+     * Removing all userStories associated with
+     * the deleted meeting.
+     */
+    var textID = this.getQuery()["_id"];
+    if (!textID) {
+        throw new Error("No textID found!");
     }
-    const UserStories = mongoose.model('UserStories');
-    await UserStories.deleteMany({textID:textID})
-})
+    const UserStories = mongoose.model("UserStories");
+    await UserStories.deleteMany({ textID: textID });
+});
 export const Text = mongoose.model("Text", textSchema);
